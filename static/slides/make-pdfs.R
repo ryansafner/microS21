5eb508d0ff352064f3af04419d2eaba1f39fb323
Ryan Safner <ryansafner@gmail.com>
2021-02-01 12:40:02 -0500

    Add 1.1 pdf

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,9 +1,11 @@
 # make-pdfs
-pagedown::chrome_print("https://microS21.classes.ryansafner.com/slides/1.1-slides.html",
+pagedown::chrome_print(input = "https://microS21.classes.ryansafner.com/slides/1.1-slides.html",
+                       output = "static/slides/1.1-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print("https://microS21.classes.ryansafner.com/slides/1.1-slides.html",
+pagedown::chrome_print(input = "https://microS21.classes.ryansafner.com/slides/1.1-slides.html",
+                       output = "static/slides/1.1-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
